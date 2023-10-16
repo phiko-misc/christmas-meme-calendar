f51207c24ec501ac6cdaef43ac00d57d136c793d
phiko-misc <phgu03@gmail.com>
2023-10-17 00:11:50 +0200

    Optimised

--- src/pages/api/[id].tsx
+++ src/pages/api/[id].tsx
@@ -2,7 +2,7 @@ import { getRandomInt } from '@utils';
 import dayjs from 'dayjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import duration from "dayjs/plugin/duration";
-import { readFile } from 'fs';
+import { readFile, readFileSync } from 'fs';
 import path from 'path';
 
 dayjs.extend(duration);
@@ -25,7 +25,7 @@ export const config = {
  * @param {number} day
  * @returns {boolean}
  */
-function handleDayCheck(day: number): boolean {
+async function handleDayCheck(day: number): Promise<boolean> {
   if (dayjs().month(11).date() >= day && dayjs().month() === 9) {
     return true;
   }
@@ -39,7 +39,7 @@ function handleDayCheck(day: number): boolean {
  * @param {number} day
  * @returns {("weekend" | "day")}
  */
-function dayOfTheWeekCheck(day: number): "weekend" | "day" {
+async function dayOfTheWeekCheck(day: number): Promise<"weekend" | "day"> {
   const dayOfTheWeek = dayjs().date(day).month(11).day();
   if (dayOfTheWeek === 0 || dayOfTheWeek === 6) {
     return "weekend";
@@ -50,67 +50,37 @@ function dayOfTheWeekCheck(day: number): "weekend" | "day" {
 /**
  * Handle the api call
  *
- * @async
  * @param {NextApiRequest} req
  * @param {NextApiResponse} res
  * @returns {*}
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query
+  const { id } = req.query;
   if (!id) {
-    res.status(500).json({ error: "Day not set" });
-    res.closed;
-    return;
+    return res.status(500).json({ error: "Day not set" });
   }
   if (+id > 24) {
-    res.status(500).json({ error: "Number is over 24" });
-    res.closed;
-    return;
+    return res.status(500).json({ error: "Number is over 24" });
   }
-  if (req.method === 'GET' && handleDayCheck(+id)) {
-    const random1 = getRandomInt(1, dayOfTheWeekCheck(+id) === "day" ? 4 : 8);
+  if (req.method === 'GET' && await handleDayCheck(+id)) {
+    const random1 = getRandomInt(1, await dayOfTheWeekCheck(+id) === "day" ? 4 : 8);
     if (random1 > 4) {
       const weekOfTheMonth = dayjs.duration("weeks").weeks();
       const random = getRandomInt(1, 2)
       const imgPath = `./src/images/weekends/${weekOfTheMonth + 1}/${random}/dev/${random1 - 4}.jpg`;
-      readFile(imgPath, (err, data) => {
-        // error handle
-        if (err) {
-          throw err;
-        }
+      const imageBuffer = readFileSync(imgPath);
+      const extensionName = path.extname(imgPath);
+      const base64Image = Buffer.from(imageBuffer).toString('base64');
+      return res.status(200).json({ image: `data:image/${extensionName.split('.').pop()};base64,${base64Image}` });
 
-        // get image file extension name
-        const extensionName = path.extname(imgPath);
 
-        // convert image file to base64-encoded string
-        const base64Image = Buffer.from(data).toString('base64');
-
-        // combine all strings
-        res.status(200).json({ image: `data:image/${extensionName.split('.').pop()};base64,${base64Image}` });
-
-      })
     }
     const imgPath = `./src/images/${+id}/dev/${random1}.jpg`;
-
-    readFile(imgPath, (err, data) => {
-      // error handle
-      if (err) {
-        throw err;
-      }
-
-      // get image file extension name
-      const extensionName = path.extname(imgPath);
-
-      // convert image file to base64-encoded string
-      const base64Image = Buffer.from(data).toString('base64');
-
-      // combine all strings
-      res.status(200).json({ image: `data:image/${extensionName.split('.').pop()};base64,${base64Image}` });
-
-    })
+    const imageBuffer = readFileSync(imgPath);
+    const base64Image = Buffer.from(imageBuffer).toString('base64');
+    const extensionName = path.extname(imgPath);
+    return res.status(200).json({ image: `data:image/${extensionName.split('.').pop()};base64,${base64Image}` });
   } else {
-    res.status(500).json({ error: "Unexpected error" });
-    res.closed;
-    return;
+    return res.status(500).json({ error: "Unexpected error" });
   }
 }
