8809d477f453b70c62a42b7e4d767834cd248aea
Phiko <phgu03@gmail.com>
2023-12-01 15:27:00 +0100

    Fixed countdown

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   output: "standalone",
   serverRuntimeConfig: {
     URL: process.env.SERVER_API_URL ?? "http://localhost:3000/api/",
-    MONTH: process.env.MONTH ?? 10,
+    MONTH: process.env.MONTH ?? 11,
   },
   publicRuntimeConfig: {
     URL: process.env.CLIENT_API_URL ?? "http://localhost:3000/api/",
-    MONTH: process.env.MONTH ?? 10,
+    MONTH: process.env.MONTH ?? 11,
   },
   experimental: {
     largePageDataBytes: 716800
