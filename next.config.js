743d3788d4113f151c568b1f1e52754bb3394f2f
Philip Guldborg <phgu03@gmail.com>
2024-02-21 20:00:29 +0100

    Fixed and better way to set month so test will no longer fail

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
+const dayjs = require('dayjs')
+const currentMonth = dayjs().month();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   serverRuntimeConfig: {
     URL: (process.env.VERCEL_URL && process.env.VERCEL_URL !== "" ? `http://${process.env.VERCEL_URL}/api/` : undefined) ?? process.env.SERVER_API_URL ?? "http://localhost:3000/api/",
-    MONTH: process.env.MONTH ?? 11,
+    MONTH: process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test" ? currentMonth : process.env.MONTH ?? 11,
   },
   publicRuntimeConfig: {
     URL: (process.env.VERCEL_URL && process.env.VERCEL_URL !== "" ? `http://${process.env.VERCEL_URL}/api/` : undefined) ?? process.env.CLIENT_API_URL ?? "http://localhost:3000/api/",
-    MONTH: process.env.MONTH ?? 11,
+    MONTH: process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test" ? currentMonth : process.env.MONTH ?? 11,
   },
   experimental: {
     largePageDataBytes: 716800,
