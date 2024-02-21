3a0b6e56b9fa8bffebc7b9a34fcf918f21d3ebd1
Philip Guldborg <phgu03@gmail.com>
2024-02-21 20:01:11 +0100

    Much better way to make test

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,51 +3,73 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
+import dayjs from "dayjs";
+
+const currentDay = dayjs().day();
+
 // Cypress E2E Test
-describe('Navigation', () => {
-    it('should navigate to the home page', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('h1').contains("Time Before Christmas")
-    })
+describe("Navigation", () => {
+  it("should navigate to the home page", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("h1").contains("Time Before Christmas");
+  });
 
-    it('Test theme switch', () => {
-      cy.visit('http://localhost:3000/')
+  it("Test theme switch", () => {
+    cy.visit("http://localhost:3000/");
 
-      cy.get('[data-cy-switchtheme="true"]').click()
-      cy.getAllLocalStorage().should((storageMap) => {
-        expect(storageMap).to.deep.equal({
-          // other origins will also be present if localStorage is set on them
-          'http://localhost:3000': {
-            'theme': 'dark'
-          },
-        })
-      })
+    cy.get('[data-cy-switchtheme="true"]').click();
+    cy.getAllLocalStorage().should((storageMap) => {
+      expect(storageMap).to.deep.equal({
+        // other origins will also be present if localStorage is set on them
+        "http://localhost:3000": {
+          theme: "dark",
+        },
+      });
+    });
 
-      cy.get('[data-cy-switchtheme="true"]').click()
-      cy.getAllLocalStorage().should((storageMap) => {
-        expect(storageMap).to.deep.equal({
-          // other origins will also be present if localStorage is set on them
-          'http://localhost:3000': {
-            'theme': 'light'
-          },
-        })
-      })
-    })
+    cy.get('[data-cy-switchtheme="true"]').click();
+    cy.getAllLocalStorage().should((storageMap) => {
+      expect(storageMap).to.deep.equal({
+        // other origins will also be present if localStorage is set on them
+        "http://localhost:3000": {
+          theme: "light",
+        },
+      });
+    });
+  });
 
-    it('Test navigate button and calender', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('[data-cy-simplebutton="true"]').click()
-      cy.get('[data-cy-calender="true"]')
-      cy.get('[data-cy-calenderbutton="1"]').click()
-      cy.get("img")
-      cy.get('.visible > [data-cy-simplebutton="true"]').click()
-    })
+  it("Test navigate button and calender", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy-simplebutton="true"]').click();
+    cy.get('[data-cy-calender="true"]');
+    cy.get('[data-cy-calenderbutton="1"]').click();
+    cy.get("img");
+    cy.get('.visible > [data-cy-simplebutton="true"]').click();
+  });
 
-    it('Test not allow', () => {
-      cy.visit('http://localhost:3000/dev/24')
-      cy.get('[data-cy-notnow="true"]').should('have.text', 'You are notready for this')
-    })
-  })
-  
-  // Prevent TypeScript from reading file as legacy script
-  export {}
+  it("Test more then one picture", () => {
+    if (currentDay > 1) {
+      cy.visit("http://localhost:3000/");
+      cy.get('[data-cy-simplebutton="true"]').click();
+      cy.get('[data-cy-calender="true"]');
+      cy.get(`[data-cy-calenderbutton="${currentDay}"]`).click();
+      cy.get("img");
+      cy.get('.visible > [data-cy-simplebutton="true"]').click();
+    }
+  });
+
+  it("Test not allow", () => {
+    if (currentDay < 24) {
+      cy.visit("http://localhost:3000/dev/24");
+      cy.get('[data-cy-notnow="true"]').should(
+        "have.text",
+        "You are notready for this",
+      );
+      return;
+    }
+    cy.log(`Current day i month is ${currentDay} TEST NOT RUN`);
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
